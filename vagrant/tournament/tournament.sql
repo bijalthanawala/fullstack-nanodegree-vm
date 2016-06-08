bbb5d0c4c7f879bee27897587195ffd83ba23aaf
bijal <bijal@localhost.localdomain>
2016-06-09 09:29:07 +1000

    Initial .sql (work in progress)

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,24 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- DROP DATABASE tournament IF EXISTS;
+
+CREATE DATABASE tournament;
+
+\c tournament;
+
+CREATE TABLE Players (
+	plid SERIAL PRIMARY KEY,
+	name VARCHAR(50)
+	);
+
+CREATE TABLE Scores (
+	plid INTEGER REFERENCES Players,
+	score INTEGER
+	);
+
+CREATE TABLE Matches (
+	plid INTEGER REFERENCES Players
+	);
+	
 
